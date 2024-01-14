d72dca5c383f1e622b3a69d3877ff78403ff97fe
saitejag4 <100602847+saitejag4@users.noreply.github.com>
2024-01-14 20:46:27 +0530

    Update.

--- src/app/components/contact-us/contact-us.component.ts
+++ src/app/components/contact-us/contact-us.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-us.component.css']
 })
 export class ContactUsComponent implements OnInit {
+  phoneNumber = '9866823959';
+  message = 'Hello, I want to chat with you!';
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  redirectToWhatsApp(): void {
+    const whatsappLink = `https://wa.me/${this.phoneNumber}?text=${encodeURIComponent(this.message)}`;
+    window.location.href = whatsappLink;
+  }
 
 }
